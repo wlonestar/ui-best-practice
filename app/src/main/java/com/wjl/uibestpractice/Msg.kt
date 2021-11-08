d240e9c788624cfdad3a3f735ec1ba896897c78d
wjl-lab <wlonestar@163.com>
2021-11-08 22:29:17 +0800

    first commit

---
+++ app/src/main/java/com/wjl/uibestpractice/Msg.kt
@@ -0,0 +1,10 @@
+package com.wjl.uibestpractice
+
+class Msg(val content: String, val type: Int) {
+
+    companion object {
+        const val TYPE_RECEIVED = 0
+        const val TYPE_SENT = 1
+    }
+
+}
